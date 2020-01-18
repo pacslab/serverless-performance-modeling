b50607f3a4d630dfb80cc597c66bd04461ddcc2c
Nima Mahmoudi <nima.mahmoudi72@gmail.com>
2020-01-18 16:00:57 -0500

    trying root for travis

--- jupyter_docker/runtest.sh
+++ jupyter_docker/runtest.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
 IMAGE_NAME=$(cat .dockername)
+DOCKER_USER=${DOCKER_USER:-jovyan}
 cd ..
-chmod +w experiments/figs
 docker run -it --rm \
     --name slsperftest \
     -v "$(pwd)":/home/jovyan \
+    --user $DOCKER_USER \
     $IMAGE_NAME /bin/bash -c 'cd experiments && python 01-parse-plot.py && python 02-overall-plots.py && ls figs/'
