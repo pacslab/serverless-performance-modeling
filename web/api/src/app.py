74c04fb173259e34796788e38458924916e79e63
Nima Mahmoudi <nima_mahmoudi@live.com>
2020-01-27 13:04:50 -0500

    added plots

--- web/api/src/app.py
+++ web/api/src/app.py
@@ -1,6 +1,14 @@
 #!python
-from flask import Flask, jsonify, request, abort, make_response
+from flask import Flask, jsonify, request, abort, make_response, Response
 from flask_cors import CORS, cross_origin
+
+# for plots
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+import io
 
 from pacsltk import perfmodel
 
@@ -21,6 +29,86 @@ def not_found(error):
 @app.errorhandler(500)
 def not_found(error):
     return make_response(jsonify({'error': 'Server error'}), 500)
+
+
+def analyze_sls(row):
+    props, _ = perfmodel.get_sls_warm_count_dist(**row)
+    return pd.Series(props)
+
+
+def plot_configs(ylab):
+    plt.legend()
+    plt.tight_layout()
+    plt.grid(True)
+    plt.xlabel("Arrival Rate (reqs/s)")
+    plt.ylabel(ylab)
+    plt.gcf().subplots_adjust(left=0.12, bottom=0.25)
+
+
+@app.route('/perfmodel/api/plots/pcold-arrival.png', methods=['GET'])
+@cross_origin()
+def pcold_plot_arrival_rate():
+    required_params = ['idleBeforeExp', 'warmServiceTime',
+                       'coldServiceTime']
+    for required_param in required_params:
+        if not request.args.get(required_param):
+            abort(400)
+
+    idleBeforeExp = float(request.args.get('idleBeforeExp'))
+    warmServiceTime = float(request.args.get('warmServiceTime'))
+    coldServiceTime = float(request.args.get('coldServiceTime'))
+
+    params = {
+        "arrival_rate": np.logspace(-3, 2, num=10),
+        "warm_service_time": warmServiceTime,
+        "cold_service_time": coldServiceTime,
+        "idle_time_before_kill": idleBeforeExp,
+    }
+    df = pd.DataFrame(data=params)
+    df = pd.concat([df, df.apply(analyze_sls, axis=1)], axis=1)
+
+    # Cold Start Probability
+    fig = plt.figure(figsize=(7, 2))
+    plt.semilogx(df['arrival_rate'], df['cold_prob'] * 100)
+
+    plot_configs("Cold Start Prob (%)")
+
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+
+@app.route('/perfmodel/api/plots/rt-arrival.png', methods=['GET'])
+@cross_origin()
+def rt_plot_arrival_rate():
+    required_params = ['idleBeforeExp', 'warmServiceTime',
+                       'coldServiceTime']
+    for required_param in required_params:
+        if not request.args.get(required_param):
+            abort(400)
+
+    idleBeforeExp = float(request.args.get('idleBeforeExp'))
+    warmServiceTime = float(request.args.get('warmServiceTime'))
+    coldServiceTime = float(request.args.get('coldServiceTime'))
+
+    params = {
+        "arrival_rate": np.logspace(-3, 2, num=10),
+        "warm_service_time": warmServiceTime,
+        "cold_service_time": coldServiceTime,
+        "idle_time_before_kill": idleBeforeExp,
+    }
+    df = pd.DataFrame(data=params)
+    df = pd.concat([df, df.apply(analyze_sls, axis=1)], axis=1)
+
+    # Cold Start Probability
+    fig = plt.figure(figsize=(7, 2))
+    plt.semilogx(df['arrival_rate'], df['avg_resp_time'])
+
+    plot_configs("RT (s)")
+
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
 
 
 @app.route('/perfmodel/api/props', methods=['POST'])
