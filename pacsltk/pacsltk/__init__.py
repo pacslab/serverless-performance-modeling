e00d2961fd20f94e4ae661ba4653791c380d8e0c
Nima Mahmoudi <nima_mahmoudi@live.com>
2020-06-24 17:01:02 -0400

    deploying on production branch only, and removed tqdm inside exp parser

--- pacsltk/pacsltk/__init__.py
+++ pacsltk/pacsltk/__init__.py
@@ -3,7 +3,7 @@ import pacsltk.perfmodel as perfmodel
 import pacsltk.pacs_util as pacs_util
 import pacsltk.exp_parser as exp_parser
 
-__version__ = '0.1.11'
+__version__ = '0.1.12'
 
 __author__ = 'Nima Mahmoudi <nma@ieee.org>'
 
